1dfdd96e0399761e076997e1a4e1a0eb4baad215
ntoarn <nqton301004@gmail.com>
2024-07-16 16:43:25 +0700

    client

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -5,7 +5,6 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./global.css"
 import "./assets/js/index.js"
-// import "./styles/style.scss";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
