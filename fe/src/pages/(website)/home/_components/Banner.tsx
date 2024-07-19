157ce21e95256e6e8658d562d8e8375d4e2eba36
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-07-20 00:32:33 +0700

    test

--- fe/src/pages/(website)/home/_components/Banner.tsx
+++ fe/src/pages/(website)/home/_components/Banner.tsx
@@ -7,21 +7,23 @@ const Banner = () => {
     <div>
       {" "}
       <section
-        className={`lg:bg-[url('./assets/images/Homepage/banner1.png')] bg-[url('./assets/images/Homepage/banner-mobile.png')] min-h-[585px] lg:min-h-[885px] lg:min-w-[1280px] bg-top bg-no-repeat lg:bg-center bg-cover mx-7 flex py-32 lg:items-center lg:py-0 mt-9 mb-24`}
+        className={`lg:bg-[url('./assets/images/Homepage/banner1.png')] bg-[url('./assets/images/Homepage/banner-mobile.png')] w-full  bg-cover bg-top `}
       >
-        {/* <!-- Banner --> */}
-        <div className="container">
-          <div>
-            <h3 className="md:text-4xl text-2xl tracking-[0.5px] font-semibold">
-              classNameic Exclusive
-            </h3>
-            <h1 className="md:text-6xl text-4xl md:my-6 my-2 font-bold">
-              Women's Collection
-            </h1>
-            <p className="uppercase md:text-3xl text-xl">upto 40% off</p>
-            <button className="btn-black md:px-6 md:py-4 px-4 py-2 mt-12 rounded-lg">
-              Shop Now <i className="fa-solid fa-arrow-right px-2"></i>
-            </button>
+        <div className=" min-h-[585px] lg:min-h-[885px] lg:min-w-[1280px]  bg-no-repeat lg:bg-center   flex py-32 lg:items-center lg:py-0 mt-9 mb-24">
+          {/* <!-- Banner --> */}
+          <div className="container">
+            <div>
+              <h3 className="md:text-4xl text-2xl tracking-[0.5px] font-semibold">
+                classNameic Exclusive
+              </h3>
+              <h1 className="md:text-6xl text-4xl md:my-6 my-2 font-bold">
+                Women's Collection
+              </h1>
+              <p className="uppercase md:text-3xl text-xl">upto 40% off</p>
+              <button className="btn-black md:px-6 md:py-4 px-4 py-2 mt-12 rounded-lg">
+                Shop Now <i className="fa-solid fa-arrow-right px-2"></i>
+              </button>
+            </div>
           </div>
         </div>
       </section>
