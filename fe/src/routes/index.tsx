4fd54b1630b943f3722f9cf7f98249f854ddc0fd
ntoarn <nqton301004@gmail.com>
2024-08-20 21:26:16 +0700

    giaiquyetxungdot

--- fe/src/routes/index.tsx
+++ fe/src/routes/index.tsx
@@ -2,33 +2,34 @@ import { Dashboard } from "@/pages/(dashboard)/dashboard/page";
 import LayoutAdmin from "@/pages/(dashboard)/layout";
 import Products from "@/pages/(dashboard)/products/page";
 import NotFoundPage from "@/pages/(website)/404/page";
-import ForgotPassword from "@/pages/(website)/ForgotPassword/ForgotPassword";
-import LoginSuccessfull from "@/pages/(website)/LoginSuccessfull/LoginSuccessfull";
-import Cart from "@/pages/(website)/cart/Cart";
 import HomePage from "@/pages/(website)/home/page";
 import LayoutWebsite from "@/pages/(website)/layout";
-import Login from "@/pages/(website)/login/login";
-import Register from "@/pages/(website)/register/register";
 import { Route, Routes } from "react-router-dom";
-import PageOur from "../pages/(website)/ourstory/pageOur";
 import Page from "./../pages/(website)/shop/shop";
-import Minicard from "@/pages/(website)/MInicard/Minicard";
-import OrderPlaceSuccess from "@/pages/(website)/OrderPlaceSuccessfully/OrderPlaceSuccessfully";
-import ShippingAddressPage from "@/pages/(website)/ShipingAdrres/ShipingAdrres";
-import ManageAddresses from "@/pages/(website)/manageaddresses/ManageAddresses";
-import MyOrder from "@/pages/(website)/myOrder/Myorder";
+import PageOur from "../pages/(website)/ourstory/pageOur";
+import Login from "@/pages/(website)/login/login";
+import Cart from "@/pages/(website)/cart/Cart";
+import Register from "@/pages/(website)/register/register";
+import ForgotPassword from "@/pages/(website)/ForgotPassword/ForgotPassword";
+import LoginSuccessfull from "@/pages/(website)/LoginSuccessfull/LoginSuccessfull";
+
 import MyProfile from "@/pages/(website)/myprofile/MyProfile";
 import MyWishlists from "@/pages/(website)/mywishlists/MyWishlists";
+import ManageAddresses from "@/pages/(website)/manageaddresses/ManageAddresses";
 import Notification from "@/pages/(website)/notifications/Notification";
-import Ordersummary from "@/pages/(website)/ordersummary/ordersummary";
-import Payment from "@/pages/(website)/payment/PayMent";
-import SavedCard from "@/pages/(website)/savedcards/SavedCard";
 import Setting from "@/pages/(website)/settings/Setting";
-import PageProductDetail from "@/pages/(website)/productdetail/PageProductDetail";
-import Test from "@/pages/(website)/test";
+import SavedCard from "@/pages/(website)/savedcards/SavedCard";
+import OrderPlaceSuccess from "@/pages/(website)/OrderPlaceSuccessfully/OrderPlaceSuccessfully";
+import Minicard from "@/pages/(website)/MInicard/Minicard";
+import MyOrder from "@/pages/(website)/myOrder/Myorder";
+import Ordersummary from "@/pages/(website)/ordersummary/ordersummary";
+import ShippingAddressPage from "@/pages/(website)/ShipingAdrres/ShipingAdrres";
+import Payment from "@/pages/(website)/payment/PayMent";
 import OTPPage from "@/pages/(website)/EnterOTP/EntOtp";
 import Contact from "@/pages/(website)/contact/Contact";
 import ScrollToTop from "@/assets/js/scrolltotop";
+import Test from "@/pages/(website)/test";
+
 const Router = () => {
   return (
     <>
@@ -39,7 +40,6 @@ const Router = () => {
         <Route path="/" element={<LayoutWebsite />}>
           <Route index element={<HomePage />} />
           <Route path="/shop" element={<Page />} />
-          <Route path="/product-detail" element={<PageProductDetail/>}/>
           <Route path="/ourstory" element={<PageOur />} />
           <Route path="/myprofile" element={<MyProfile />} />
           <Route path="/mywishlist" element={<MyWishlists />} />
