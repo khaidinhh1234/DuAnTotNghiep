f2e3cde618335d42d918de398055c3fe4beadd8d
ntoarn <nqton301004@gmail.com>
2024-08-22 01:50:24 +0700

    dk dn

---
+++ fe/src/common/types/user.ts
@@ -0,0 +1,16 @@
+export interface IUser {
+    _id?: number | string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    confirmPassword?: string;
+    phoneNumber?: string;
+    avatar?: string;
+    address?: string;
+    role?: "admin" | "member" | "guest";
+    createdAt?: Date;
+    updatedAt?: Date;
+    isActive?: boolean;
+  }
+  
--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -4,17 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { StyleProvider } from "@ant-design/cssinjs";
 
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./global.css";
 // import "./assets/js/index.js";
-
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
       <StyleProvider layer>
         <App />
       </StyleProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
