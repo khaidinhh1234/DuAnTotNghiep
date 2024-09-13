25103bcd8fc3f51c09a4b9f926f985a19e867636
kien48 <kienltph35295@fpt.edu.vn>
2024-09-13 12:36:10 +0700

    Quản lý thông tin web

--- be/database/migrations/2024_09_09_023322_create_thong_tin_webs_table.php
+++ be/database/migrations/2024_09_09_023322_create_thong_tin_webs_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
     {
         Schema::create('thong_tin_webs', function (Blueprint $table) {
             $table->id();
-            $table->string('ten_website')->nullable();
-            $table->string('logo_website')->nullable();
-            $table->string('ten_doanh_nghiep')->nullable();
-            $table->string('dia_chi')->nullable();
-            $table->string('email')->nullable();
-            $table->string('so_dien_thoai_dat_hang', 15)->nullable();
-            $table->string('so_dien_thoai_khieu_nai', 15)->nullable();
+            $table->string('ten_website');
+            $table->string('logo_website');
+            $table->string('ten_doanh_nghiep');
+            $table->string('dia_chi');
+            $table->string('email');
+            $table->string('so_dien_thoai_dat_hang', 15);
+            $table->string('so_dien_thoai_khieu_nai', 15);
             $table->string('cau_noi')->nullable();
+            $table->json('banner');
             $table->string('link_facebook')->nullable();
             $table->string('link_youtube')->nullable();
             $table->string('link_zalo')->nullable();
