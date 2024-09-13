886433605d7c39c3f9bdac946e16438c8bc938be
Nguyễn Chí Đức <lovemchau@gmail.com>
2024-09-13 21:36:40 +0700

    ducnc/fixforgotpass

--- be/resources/views/emails/password-reset.blade.php
+++ be/resources/views/emails/password-reset.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body >
