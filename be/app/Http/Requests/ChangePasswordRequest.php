d77011499705b44f677a1484d2aeddf2a5ce26eb
vuviet071104gmail <144698723+vuviet071104gmail@users.noreply.github.com>
2024-09-13 21:41:01 +0700

    changepassword

---
+++ be/app/Http/Requests/ChangePasswordRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'current_password' => 'required',
+            'new_password' => 'required|min:8|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'current_password.required' => 'Mật khẩu hiện tại là bắt buộc.',
+            'new_password.required' => 'Mật khẩu mới là bắt buộc.',
+            'new_password.min' => 'Mật khẩu mới phải có ít nhất 8 ký tự.',
+            'new_password.confirmed' => 'Mật khẩu mới và xác nhận mật khẩu không khớp.',
+        ];
+    }
+}
