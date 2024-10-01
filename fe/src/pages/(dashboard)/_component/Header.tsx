309abff6cedff985ce2cdad534783dc557527284
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-10-01 16:54:30 +0700

    khai/chitietdonhang

--- fe/src/pages/(dashboard)/_component/Header.tsx
+++ fe/src/pages/(dashboard)/_component/Header.tsx
@@ -34,7 +34,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="flex h-14 items-center gap-4 border-b  px-4 lg:h-[60px] lg:px-6">
+    <header className="sticky top-0 z-50 flex h-14 items-center gap-4 border-b bg-white px-4 lg:h-[60px] lg:px-6">
       <Sheet>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="shrink-0 md:hidden">
@@ -109,16 +109,7 @@ const Header = () => {
         </SheetContent>
       </Sheet>
       <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search products..."
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
+        <form></form>
       </div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
--- fe/src/pages/(dashboard)/layout.tsx
+++ fe/src/pages/(dashboard)/layout.tsx
@@ -5,7 +5,9 @@ import SlibarProduct from "./_component/Sliber";
 export function LayoutAdmin() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[320px_1fr] ">
-      <SlibarProduct />
+      <div className="">
+        <SlibarProduct />
+      </div>
       <div className="flex flex-col bg-[#f3f2f2]">
         <Header />
         <Outlet />
