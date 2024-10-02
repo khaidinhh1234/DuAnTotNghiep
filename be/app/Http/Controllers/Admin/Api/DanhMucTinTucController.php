5c9baab747669da60c5526ae25dde0cea9f8cffe
kdat2108 <nguyenkimdat2108@gmail.com>
2024-10-02 18:55:06 +0700

    Them moi danh muc tin tuc

--- be/app/Http/Controllers/Admin/Api/DanhMucTinTucController.php
+++ be/app/Http/Controllers/Admin/Api/DanhMucTinTucController.php
@@ -35,4 +35,37 @@ class DanhMucTinTucController extends Controller
             ], 500);
         }
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $validateDanhMucTinTuc = $request->validate([
+                'ten_danh_muc_tin_tuc' => 'required|string|max:255',
+            ]);
+            $validateDanhMucTinTuc['duong_dan'] = Str::slug($validateDanhMucTinTuc['ten_danh_muc_tin_tuc']);
+            $danhMucTinTuc = DanhMucTinTuc::create($validateDanhMucTinTuc);
+            DB::commit();
+            return response()->json(
+                [
+                    'status' => true,
+                    'status_code' => 200,
+                    'message' => 'Danh mục tin tức đã được thêm thành công',
+                    'data' => $danhMucTinTuc,
+                ],
+                200
+            );
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return response()->json([
+                'status' => false,
+                'status_code' => 500,
+                'message' => 'Đã có lỗi xảy ra khi thêm danh mục tin tức',
+                'error' => $exception->getMessage()
+            ], 500);
+        }
+    }
 }
