dc20221743a7acf1cbaed66e471bc2cd0d5d7d7a
kdat2108 <nguyenkimdat2108@gmail.com>
2024-10-02 17:11:02 +0700

    Danh sách danh mục

--- be/app/Http/Controllers/Client/Api/LienHeController.php
+++ be/app/Http/Controllers/Client/Api/LienHeController.php
@@ -11,46 +11,46 @@ use Illuminate\Support\Facades\Auth;
 class LienHeController extends Controller
 {
 
-    public function lienHe(Request $request)
-    {
-        try {
-            $validateLienHe = $request->validate([
-                'name' => 'required|string|max:255',
-                'email' => 'required|email|max:255',
-                'noi_dung_lien_he' => 'required',
-                'sdt_lien_he' => 'nullable|string|max:20',
-                'loai_lien_he' => 'nullable|string',
-                'trang_thai_lien_he' => 'nullable|string',
-            ]);
+    // public function lienHe(Request $request)
+    // {
+    //     try {
+    //         $validateLienHe = $request->validate([
+    //             'name' => 'required|string|max:255',
+    //             'email' => 'required|email|max:255',
+    //             'noi_dung_lien_he' => 'required',
+    //             'sdt_lien_he' => 'nullable|string|max:20',
+    //             'loai_lien_he' => 'nullable|string',
+    //             'trang_thai_lien_he' => 'nullable|string',
+    //         ]);
 
 
-            if (Auth::guard('api')->check()) {
-                $validateLienHe['user_id'] = Auth::guard('api')->id();
-            } else {
-                return response()->json([
-                    'status' => false,
-                    'status_code' => 401,
-                    'message' => 'Không thể gửi liên hệ',
-                    'data' => Auth::check()
-                ], 401);
-            }
+    //         if (Auth::guard('api')->check()) {
+    //             $validateLienHe['user_id'] = Auth::guard('api')->id();
+    //         } else {
+    //             return response()->json([
+    //                 'status' => false,
+    //                 'status_code' => 401,
+    //                 'message' => 'Không thể gửi liên hệ',
+    //                 'data' => Auth::check()
+    //             ], 401);
+    //         }
 
-            $lienhe = LienHe::create($validateLienHe);
-            event(new SendMail( $lienhe->email, $lienhe->name, 'contact'));
-            return response()->json([
-                'status' => true,
-                'status_code' => 200,
-                'message' => 'Gửi liên hệ thành công',
-                'data' => $lienhe
-            ]);
-        } catch (\Exception $exception) {
+    //         $lienhe = LienHe::create($validateLienHe);
+    //         event(new SendMail( $lienhe->email, $lienhe->name, 'contact'));
+    //         return response()->json([
+    //             'status' => true,
+    //             'status_code' => 200,
+    //             'message' => 'Gửi liên hệ thành công',
+    //             'data' => $lienhe
+    //         ]);
+    //     } catch (\Exception $exception) {
 
-            return response()->json([
-                'status' => false,
-                'status_code' => 500,
-                'message' => 'Đã có lỗi xảy ra khi gửi liên hệ',
-                'error' => $exception->getMessage()
-            ], 500);
-        }
-    }
+    //         return response()->json([
+    //             'status' => false,
+    //             'status_code' => 500,
+    //             'message' => 'Đã có lỗi xảy ra khi gửi liên hệ',
+    //             'error' => $exception->getMessage()
+    //         ], 500);
+    //     }
+    // }
 }
