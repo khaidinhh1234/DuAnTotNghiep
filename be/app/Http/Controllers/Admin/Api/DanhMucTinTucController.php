b6ed24d2fa502d0827bb2803622a1d2ce3f907c0
kdat2108 <nguyenkimdat2108@gmail.com>
2024-10-02 18:52:36 +0700

    Danh sach Danh Muc Tin Tuc

--- be/app/Http/Controllers/Admin/Api/DanhMucTinTucController.php
+++ be/app/Http/Controllers/Admin/Api/DanhMucTinTucController.php
@@ -10,5 +10,29 @@ use Illuminate\Support\Str;
 
 class DanhMucTinTucController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $danhMucTinTucs = DanhMucTinTuc::orderBy('created_at', 'desc')->get();
+            return response()->json(
+                [
+                    'status' => true,
+                    'status_code' => 200,
+                    'message' => 'Lấy dữ liệu thành công',
+                    'data' => $danhMucTinTucs,
+                ],
+                200
+            );
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'status_code' => 500,
+                'message' => 'Đã có lỗi xảy ra khi lấy dữ liệu',
+                'error' => $exception->getMessage()
+            ], 500);
+        }
+    }
 }
