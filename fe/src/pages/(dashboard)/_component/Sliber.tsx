f8322bd2c10fd2cf7e7f631d4af94bfb1393407f
ntoarn <nqton301004@gmail.com>
2024-10-05 16:02:02 +0700

    pull

--- fe/src/pages/(dashboard)/_component/Sliber.tsx
+++ fe/src/pages/(dashboard)/_component/Sliber.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { Button, Layout, Menu, MenuProps } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
+import { Footer } from "antd/es/layout/layout";
 import {
-  Home,
-  Package,
-  ShoppingCart,
-  Users,
-  CheckCircle,
   AlignJustify,
+  BarChart,
+  CheckCircle,
+  FileText,
+  Folder,
+  Grid,
+  Home,
+  Image,
+  Layers,
   LineChart,
+  List,
+  ListChecks,
+  Lock,
+  Newspaper,
+  Package,
+  PieChart,
+  Shield,
+  ShoppingCart,
+  Star,
+  Tag,
+  Truck,
+  User,
+  UserCheck,
+  Users
 } from "lucide-react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Navigate } from "react-router";
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -40,31 +57,31 @@ const items: MenuItem[] = [
     getItem("Biến thể", "3", null, "/admin/products/bienthe"),
     getItem("Quản lý nhãn dán", "4", null, "/admin/products/tags"),
   ]),
-  getItem("Danh mục", "sub2", <Package />, "", [
+  getItem("Danh mục", "sub2", <Folder />, "", [
     getItem("Danh mục sản phẩm", "5", null, "/admin/categories"),
     getItem("Danh mục tin tức", "6", null, "/admin/newcategory"),
   ]),
-  getItem('Đơn hàng', 'sub3', <Package />, '', [
+  getItem("Đơn hàng", "sub3", <ShoppingCart />, "", [
     getItem('Tổng quan', '7', null, '/admin/orders/transport'),
     getItem('Danh sách đơn hàng', '8', null, '/admin/orders/list'),
     getItem('Vận chuyển', '9', null, '/admin/orders/uncomfirmedorder'),
-    getItem('Thu hộ', '19', null, '/admin/orders/collect'),
+    // getItem('Thu hộ', '19', null, '/admin/orders/collect'),
   ]),
-  getItem("Tin tức", "20", <Package />, "/admin/news"),
-  getItem("Khuyến mãi", "10", <Package />, "/admin/vouchers"),
-  getItem("Tài khoản", "sub4", <Package />, "", [
+  getItem("Tin tức", "20", <Newspaper />, "/admin/news"), 
+  getItem("Tài khoản", "sub4", <User />, "", [
     getItem("Khách hàng", "11", null, "/admin/users/khachhang"),
     getItem("Nhân viên", "12", null, "/admin/users/nhanvien"),
     getItem("Hạng thành viên", "13", null, "/admin/users/rank"),
   ]),
-  getItem("Đánh giá", "14", <CheckCircle />, "/admin/evaluates"),
-  getItem("Phân quyền", "15", <CheckCircle />, "/admin/ADmin/userprivileges"),
+  getItem("Đánh giá", "14", <Star />, "/admin/evaluates"), 
+  getItem("Phân quyền", "15", <Lock />, "/admin/ADmin/userprivileges"), 
   getItem("Nội dung", "sub5", <AlignJustify />, "", [
     getItem("Footer", "16", null, "/admin/Content/qlfooter"),
     getItem("Banner", "17", null, "/admin/Content/qlbanner"),
   ]),
-  getItem("Thống kê", "18", <LineChart />, "/admin/analytics"),
+  getItem("Thống kê", "18", <BarChart />, "/admin/analytics"), 
 ];
+
 
 const SiderComponent: React.FC = () => {
   const navigate = useNavigate();
