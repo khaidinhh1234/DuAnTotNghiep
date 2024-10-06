a70f14772df3c13de4388d86e85e65b572dd80af
kien48 <kienltph35295@fpt.edu.vn>
2024-10-07 00:26:32 +0700

    update banner

---
+++ be/database/migrations/2024_10_07_000535_add_loai_kich_thuoc_to_bien_the_kich_thuocs_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bien_the_kich_thuocs', function (Blueprint $table) {
+            $table->enum('loai_kich_thuoc', ['nguoi_lon', 'tre_em'])->after('kich_thuoc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bien_the_kich_thuocs', function (Blueprint $table) {
+            $table->dropColumn('loai_kich_thuoc');
+        });
+    }
+};
