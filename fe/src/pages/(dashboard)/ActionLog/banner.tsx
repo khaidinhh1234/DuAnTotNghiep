7086018d94214f56f3445dab63e626bc809fd51b
chi-duc-1611 <ducncph32766@fpt.edu.vn>
2024-10-11 16:30:12 +0700

    push

--- fe/src/pages/(dashboard)/ActionLog/banner.tsx
+++ fe/src/pages/(dashboard)/ActionLog/banner.tsx
@@ -4,6 +4,7 @@ import {
   YoutubeFilled,
   TwitterCircleFilled,
   UserOutlined,
+  FileTextOutlined,
 } from "@ant-design/icons";
 // import profilecover from 'src/assets/images/backgrounds/profilebg.jpg';
 // import userimg from 'src/assets/images/profile/user-1.jpg';
@@ -12,12 +13,14 @@ import {
 const { Text, Title } = Typography;
 
 const Banner = () => {
+  const url =
+    "https://res-console.cloudinary.com/dpypwbeis/thumbnails/v1/image/upload/v1728619647/Ml9vaTY1bXY=/drilldown";
   return (
     <>
       <Card
         cover={
           <img
-            src="http://localhost:5174/src/assets/images/backgrounds/profilebg.jpg"
+            src="https://res.cloudinary.com/dpypwbeis/image/upload/v1728621916/1000_F_620906721_ReTaV6nVI1CXbmb5pG0uMjz7TmrYXo3z_fm1usn.jpg"
             alt="Profile Cover"
             className="w-full h-48 object-cover"
           />
@@ -34,11 +37,13 @@ const Banner = () => {
           >
             <div className="flex justify-between w-full max-w-xs space-x-6 text-center">
               <div>
-                <UserOutlined style={{ fontSize: "24px", color: "#50b2fc" }} />
+                <FileTextOutlined
+                  style={{ fontSize: "24px", color: "#50b2fc" }}
+                />
                 <Title level={4} className="m-0">
                   938
                 </Title>
-                <Text type="secondary">Posts</Text>
+                <Text type="secondary">Bài viết</Text>
               </div>
               <div>
                 <UserOutlined style={{ fontSize: "24px", color: "#50b2fc" }} />
@@ -65,10 +70,11 @@ const Banner = () => {
           >
             <div className="-mt-20 flex flex-col items-center">
               <Avatar
-                src="https://asset.cloudinary.com/dpypwbeis/52ee53e558237836cb23e724332e3918"
+                src={url}
                 size={110}
                 className="border-4 border-white shadow-lg"
               />
+
               <div className="mt-3 text-center">
                 <Title level={5} className="m-0">
                   Ngô Đình Toản
@@ -100,7 +106,6 @@ const Banner = () => {
                 icon={<YoutubeFilled className="text-white" />}
                 style={{ backgroundColor: "#CD201F" }}
               />
-              <Button type="primary">Add To Story</Button>
             </div>
           </Col>
         </Row>
--- fe/src/pages/(dashboard)/ActionLog/page.tsx
+++ fe/src/pages/(dashboard)/ActionLog/page.tsx
@@ -1,65 +1,164 @@
 import { Col, Table, Tag } from "antd";
 import ProfileBanner from "../adminProfile/profile/ProfileBanner";
 import Banner from "./banner";
-
-// Dữ liệu mẫu cho bảng
-const data = [
-  {
-    key: "1",
-    name: "Nguyễn Văn A",
-    email: "a@example.com",
-    loginTime: "2024-10-10 10:30",
-    status: "Đã đăng nhập",
-  },
-  {
-    key: "2",
-    name: "Trần Thị B",
-    email: "b@example.com",
-    loginTime: "2024-10-11 09:15",
-    status: "Đã đăng nhập",
-  },
-  {
-    key: "3",
-    name: "Lê Văn C",
-    email: "c@example.com",
-    loginTime: "2024-10-11 08:45",
-    status: "Đã đăng xuất",
-  },
-];
-
-// Cột cho bảng
-const columns = [
-  {
-    title: "Tên",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Thời gian đăng nhập",
-    dataIndex: "loginTime",
-    key: "loginTime",
-  },
-  {
-    title: "Trạng thái",
-    dataIndex: "status",
-    key: "status",
-    render: (text: any) => {
-      let color = text === "Đã đăng nhập" ? "green" : "volcano";
-      return (
-        <Tag color={color} key={text}>
-          {text}
-        </Tag>
-      );
-    },
-  },
-];
-// type SelectCommonPlacement = SelectProps["placement"];
+import { useQuery } from "@tanstack/react-query";
+import instance from "@/configs/admin";
 export function ActionLog() {
+  const { data: lichsu } = useQuery({
+    queryKey: ["actionLog"],
+    queryFn: async () => {
+      const response = await instance.get("lich-su-hoat-dong");
+
+      return response.data;
+    },
+  });
+  const tableNamesWithAccents: { [key: string]: string } = {
+    lien_hes: "Liên hệ",
+    anh_bien_thes: "Ảnh biến thể",
+    bien_the_san_phams: "Biến thể sản phẩm",
+    san_phams: "Sản phẩm",
+    danh_mucs: "Danh mục",
+    don_hangs: "Đơn hàng",
+    tai_khoans: "Tài khoản",
+    danh_muc_tin_tucs: "Danh mục tin tức",
+    tin_tucs: "Tin tức",
+  };
+
+  console.log(lichsu);
+
+  const datas = lichsu?.map((item: any, index: number) => {
+    const date = new Date(item?.created_at); // Chuyển đổi chuỗi thành đối tượng Date
+
+    // Chuyển sang múi giờ Việt Nam (GMT+7)
+    const vietnamTime = new Date(date.getTime() + 7 * 60 * 60 * 1000);
+    return {
+      key: index + 1,
+      ten_bang: tableNamesWithAccents[item?.ten_bang] || item?.ten_bang,
+      hanh_dong: item?.loai_thao_tac,
+      anh: item?.user?.anh_nguoi_dung,
+      email: item?.user?.email,
+      thoi_gian: `${vietnamTime.toLocaleDateString()} ${vietnamTime.toLocaleTimeString()}`, // Định dạng theo ngày và giờ
+      nhanvien: item?.user?.ho + " " + item?.user?.ten,
+    };
+  });
+  // console.log(datas);
+  // Dữ liệu mẫu cho bảng
+  // const data = [
+  //   {
+  //     key: "1",
+  //     name: "Nguyễn Văn A",
+  //     hanh_dong: "xóa",
+  //     loginTime: "2024-10-10 10:30",
+  //     status:
+  //       "Hướng dẫn quản lý kế toán dịch vụ xem lại lịch sử thao tác của nhân viên trên ASP: Biết được cụ thể",
+  //   },
+  //   {
+  //     key: "2",
+  //     name: "Trần Thị B",
+  //     hanh_dong: "thêm",
+  //     loginTime: "2024-10-11 09:15",
+  //     status:
+  //       "Hướng dẫn quản lý kế toán dịch vụ xem lại lịch sử thao tác của nhân viên trên ASP: Biết được cụ thể",
+  //   },
+  //   {
+  //     key: "3",
+  //     name: "Lê Văn C",
+  //     hanh_dong: "cập nhật",
+  //     loginTime: "2024-10-11 08:45",
+  //     status:
+  //       "Hướng dẫn quản lý kế toán dịch vụ xem lại lịch sử thao tác của nhân viên trên ASP: Biết được cụ thể",
+  //   },
+  // ];
+
+  // Cột cho bảng
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "key",
+      key: "key",
+      className: "w-[5%]",
+    },
+    {
+      title: "Tài khoản",
+
+      key: "name",
+      className: "w-[30%] ",
+
+      render: (text: any) => {
+        // console.log(text);
+        return (
+          <div className="flex items-center ">
+            <img
+              src={text?.anh ? text?.anh : "/images/avatar.jpg"}
+              alt="avatar"
+              className="w-10 h-10 rounded-full"
+            />
+            <div className="ml-3">
+              <div className="font-semibold">
+                {text.nhanvien ? text.nhanvien : "không có"}
+              </div>
+              <div className="text-gray-500">
+                <span className="font-semibold">Email :</span>
+                <span className="ml-1">
+                  <a href={`mailto: ${text.email ? text.email : "không có"}`}>
+                    {text.email ? text.email : "không có"}
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      },
+    },
+    {
+      title: "Bảng thao tác",
+      dataIndex: "ten_bang",
+      className: "w-[10%]",
+      key: "ten_bang",
+    },
+    {
+      title: "Hành động",
+      dataIndex: "hanh_dong",
+      render: (text: any) => {
+        let color =
+          text === "Thêm mới" ? "green" : text === "Cập nhật" ? "blue" : "red";
+        return (
+          <Tag color={color} key={text}>
+            {text}
+          </Tag>
+        );
+      },
+      className: "w-[15%]",
+      key: "hanh_dong",
+    },
+    {
+      title: "Thời gian",
+      dataIndex: "thoi_gian",
+      className: "w-[20%]",
+      key: "thoi_gian",
+    },
+
+    {
+      title: "Mô tả chi tiết",
+      dataIndex: "status",
+      key: "status",
+    },
+    // {
+    //   title: "Trạng thái",
+    //   dataIndex: "status",
+    //   key: "status",
+    //   render: (text: any) => {
+    //     let color = text === "Đã đăng nhập" ? "green" : "volcano";
+    //     return (
+    //       <Tag color={color} key={text}>
+    //         {text}
+    //       </Tag>
+    //     );
+    //   },
+    // },
+  ];
+  // type SelectCommonPlacement = SelectProps["placement"];
+
   return (
     <div className="flex min-h-screen w-full flex-col">
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
@@ -74,7 +173,11 @@ export function ActionLog() {
             </h1>
           </div>
           <div className="shadow-md shadow-slate-400 border-t rounded-lg">
-            <Table columns={columns} dataSource={data} />
+            <Table
+              columns={columns}
+              dataSource={datas}
+              pagination={{ pageSize: 10, className: "my-5" }}
+            />
           </div>
         </div>
       </main>
