b298e7a8747c590d8c68f544673de901639881c8
kien48 <kienltph35295@fpt.edu.vn>
2024-10-11 21:00:48 +0700

    update lshd

--- be/app/Http/Controllers/Admin/Api/LichSuHoatDongController.php
+++ be/app/Http/Controllers/Admin/Api/LichSuHoatDongController.php
@@ -10,8 +10,8 @@ class LichSuHoatDongController extends Controller
     public function index()
     {
         $data = LichSuHoatDong::query()
-            ->select('id', 'ten_bang','bang_id', 'loai_thao_tac', 'nguoi_thao_tac','created_at')
-            ->with('user')->orderByDesc('id')->get();
+            ->select('id', 'ten_bang','bang_id', 'loai_thao_tac', 'nguoi_thao_tac','mo_ta','dia_chi_ip','created_at')
+            ->with('user.vaiTros')->orderByDesc('id')->get();
         return response()->json($data);
     }
 
