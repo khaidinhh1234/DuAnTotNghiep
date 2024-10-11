7aa66d53ad81edeb13067e4b566b8817b81fde84
chi-duc-1611 <ducncph32766@fpt.edu.vn>
2024-10-11 17:01:08 +0700

    duc/ducpro

--- fe/src/pages/(website)/myOrder/_components/product.tsx
+++ fe/src/pages/(website)/myOrder/_components/product.tsx
@@ -1,5 +1,4 @@
-import { sanPham2 } from '@/assets/img';
-import React from 'react';
+import { sanPham2 } from "@/assets/img";
 
 // Component hiển thị thông tin sản phẩm
 const ProductItem = ({ status, price }) => {
@@ -7,28 +6,36 @@ const ProductItem = ({ status, price }) => {
     <div className="pb-6 flex justify-between border-b border-hrBlack">
       <div className="grid justify-between">
         <div className="flex gap-5 items-center">
-          <img
-            src={sanPham2}
-            alt="Sản phẩm"
-            className="w-12 h-12"
-          />
+          <img src={sanPham2} alt="Sản phẩm" className="w-12 h-12" />
           <div className="px-1">
             <h3 className="font-bold my-1">Váy In Họa Tiết Moana Hồng</h3>
-            <p className={`font-bold md:hidden block ${status === 'Delivered' ? '' : 'hidden'}`}>
+            <p
+              className={`font-bold md:hidden block ${status === "Delivered" ? "" : "hidden"}`}
+            >
               Giá: ${price}
             </p>
-            <p>Size: <span>S</span></p>
+            <p>
+              Size: <span>S</span>
+            </p>
             <p>Số lượng: 1</p>
           </div>
         </div>
         <div className="mt-9 px-7">
-          <a href="#" className={`text-xs px-2 py-1 rounded-sm ${status === 'Delivered' ? 'delivered' : 'inprocrass'}`}>
-            {status === 'Delivered' ? 'Đã Giao' : 'Đang Xử Lý'}
+          <a
+            href="#"
+            className={`text-xs px-2 py-1 rounded-sm ${status === "Delivered" ? "delivered" : "inprocrass"}`}
+          >
+            {status === "Delivered" ? "Đã Giao" : "Đang Xử Lý"}
           </a>
-          <span>Sản phẩm của bạn đã {status === 'Delivered' ? 'được giao' : 'đang được xử lý'}</span>
+          <span>
+            Sản phẩm của bạn đã{" "}
+            {status === "Delivered" ? "được giao" : "đang được xử lý"}
+          </span>
         </div>
       </div>
-      <div className={`text-center py-8 font-bold ${status === 'Delivered' ? 'md:block hidden' : ''}`}>
+      <div
+        className={`text-center py-8 font-bold ${status === "Delivered" ? "md:block hidden" : ""}`}
+      >
         <p>${price}</p>
       </div>
       <div className="hidden sm:block">
@@ -38,10 +45,12 @@ const ProductItem = ({ status, price }) => {
         <br />
         <button
           className={`${
-            status === 'Delivered' ? 'bg-[#FF7262] hover:bg-[#e9b2ac]' : 'bg-[#FF7262] hover:bg-[#e9b2ac]'
+            status === "Delivered"
+              ? "bg-[#FF7262] hover:bg-[#e9b2ac]"
+              : "bg-[#FF7262] hover:bg-[#e9b2ac]"
           } shadow-lg shadow-slate-600/50 text-white px-5 text-sm py-3 rounded-lg`}
         >
-          {status === 'Delivered' ? 'Hủy Đơn Hàng' : 'Hủy Đơn Hàng'}
+          {status === "Delivered" ? "Hủy Đơn Hàng" : "Hủy Đơn Hàng"}
         </button>
       </div>
     </div>
