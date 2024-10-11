2870c4f42f996807e0c977c22a533c0ecae7f1f9
Nguyễn Chí Đức <ducncph32766@fpt.edu.vn>
2024-10-12 01:28:41 +0700

    timezone

--- be/app/Http/Controllers/Admin/Api/LichSuHoatDongController.php
+++ be/app/Http/Controllers/Admin/Api/LichSuHoatDongController.php
@@ -10,7 +10,7 @@ class LichSuHoatDongController extends Controller
     public function index()
     {
         $data = LichSuHoatDong::query()
-            ->select('id', 'ten_bang','bang_id', 'loai_thao_tac', 'nguoi_thao_tac','mo_ta','dia_chi_ip','created_at')
+            ->select('id', 'ten_bang', 'bang_id', 'loai_thao_tac', 'nguoi_thao_tac', 'mo_ta', 'dia_chi_ip', 'created_at')
             ->with('user.vaiTros')->orderByDesc('id')->get();
         return response()->json($data);
     }
