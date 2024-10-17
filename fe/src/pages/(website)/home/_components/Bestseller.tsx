cd9e03160b4e364c65b50fb60888275d991bf943
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-10-17 19:57:17 +0700

    push

--- fe/src/pages/(website)/home/_components/Bestseller.tsx
+++ fe/src/pages/(website)/home/_components/Bestseller.tsx
@@ -1,22 +1,23 @@
 import { sanPham2 } from "@/assets/img";
-import instanceClient from "@/configs/client";
+import instance from "@/configs/client";
 import { useQuery } from "@tanstack/react-query";
 
 import { Link } from "react-router-dom";
 
-const Bestseller = () => {
-  const { data } = useQuery({
-    queryKey: ["PRODUCTS_KEY"],
-    queryFn: async () => {
-      const response = await instanceClient.get("trangchu");
-      if (response.data.status_code !== 200) {
-        throw new Error("Error fetching product");
-      }
-      return response.data;
-    },
-  });
-  const products = data?.danh_sach_san_pham_moi || [];
-  console.log(products);
+const Bestseller = ({ products }: any) => {
+  // const { data } = useQuery({
+  //   queryKey: ["PRODUCTS_KEYS"],
+  //   queryFn: async () => {
+  //     const response = await instance.get("trangchu");
+  //     if (response.data.status_code !== 200) {
+  //       throw new Error("Error fetching product");
+  //     }
+  //     return response.data;
+  //   },
+  // });
+  // // console.log(data);
+  // const products = data?.danh_sach_san_pham_moi || [];
+  // console.log(products);
 
   return (
     <>
@@ -25,7 +26,7 @@ const Bestseller = () => {
         <div className="container mb-28">
           <div className="flex justify-center mb-5">
             <h1 className="md:text-4xl text-3xl font-semibold tracking-[1px]">
-              Sản Phẩm Bán Chạy
+              Sản Phẩm Mới Nhất
             </h1>
           </div>
 
@@ -39,19 +40,26 @@ const Bestseller = () => {
                 <div className="product-card hover:bg-zinc-100 rounded-md shadow-lg shadow-black/10">
                   <div className="relative w-full h-[385px] ">
                     <a href="#">
-                      <i className="fa-regular fa-star text-lg bg-white hover:bg-black hover:text-white w-12 h-12 flex items-center justify-center absolute top-3 right-6 btn invisible opacity-0 transition-opacity duration-300 rounded-full" />
+                      <i className="z-20 fa-regular fa-heart text-xl pt-1 bg-white hover:bg-black hover:text-white w-11 h-11 flex items-center justify-center absolute top-3 right-6 btn invisible opacity-0 transition-opacity duration-300 rounded-full" />
                     </a>
                     <a href="#">
-                      <i className="fa-solid fa-arrow-right-arrow-left text-lg bg-white hover:bg-black hover:text-white w-12 h-12 flex items-center justify-center absolute top-[63px] right-6 btn invisible opacity-0 transition-opacity duration-300 rounded-full" />
+                      <i className="z-20 fa-solid fa-arrow-right-arrow-left text-lg bg-white hover:bg-black hover:text-white w-11 h-11 flex items-center justify-center absolute top-[63px] right-6 btn invisible opacity-0 transition-opacity duration-300 rounded-full" />
                     </a>
                     <Link to={`/product-detail/${product.id}`}>
-                      <i className="fa-regular fa-eye text-lg bg-white hover:bg-black hover:text-white w-12 h-12 flex items-center justify-center absolute top-[115px] right-6 btn invisible opacity-0 transition-opacity duration-300 rounded-full" />
+                      <i className="z-20 fa-regular fa-eye text-lg bg-white hover:bg-black hover:text-white w-11 h-11 flex items-center justify-center absolute top-[115px] right-6 btn invisible opacity-0 transition-opacity duration-300 rounded-full" />
                     </Link>
-                    <img
-                      src={product?.anh_san_pham}
-                      alt=""
-                      className="w-[300px] h-[380px] rounded-t-md"
-                    />
+                    <div className="relative">
+                      <img
+                        src={product?.anh_san_pham}
+                        alt=""
+                        className="w-[300px] h-[380px] rounded-t-md"
+                      />
+                      {product?.gia_tot == 1 && (
+                        <span className="absolute top-3 right-3 bg-red-500 text-white px-3 py-[2px] rounded-lg font-bold">
+                          new
+                        </span>
+                      )}
+                    </div>
                     <button className="hover:bg-blackL hover:text-white absolute px-[65px] py-3 left-4 rounded-lg bottom-5 bg-white invisible opacity-30 transition-opacity btn duration-300">
                       Thêm vào giỏ hàng
                     </button>
@@ -63,9 +71,15 @@ const Bestseller = () => {
                       </h5>
                     </a>
                     <p className="font-semibold text-lg">
-                      20.034.000 đ
+                      {(product?.bien_the_san_pham[0].gia_khuyen_mai).toLocaleString(
+                        "vi-VN"
+                      ) ?? 0}{" "}
+                      đ
                       <span className="text-black/20 line-through pl-3 text-[16px]">
-                        25.003.033 đ
+                        {(product?.bien_the_san_pham[0]?.gia_ban).toLocaleString(
+                          "vi-VN"
+                        ) ?? 0}{" "}
+                        đ
                       </span>
                     </p>
 
