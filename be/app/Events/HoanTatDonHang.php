688708883e70d5b2a44708502c96c6b325fd2281
kien48 <kienltph35295@fpt.edu.vn>
2024-10-21 22:13:16 +0700

    cập nhật giỏ hàng v2

---
+++ be/app/Events/HoanTatDonHang.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\DonHang;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class HoanTatDonHang implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public $donHang;
+
+    public $email;
+    public function __construct(DonHang $donHang, $email)
+    {
+        $this->$donHang = $donHang;
+        $this->$email = $email;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
