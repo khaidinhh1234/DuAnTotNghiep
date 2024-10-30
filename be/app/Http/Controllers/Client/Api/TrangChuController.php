b17599a05cd663601f86c5e34b33f3020a910c3c
kien48 <kienltph35295@fpt.edu.vn>
2024-10-30 23:00:06 +0700

    fix bug giỏ hàng và update tìm kiếm gợi ý

--- be/app/Http/Controllers/Client/Api/TrangChuController.php
+++ be/app/Http/Controllers/Client/Api/TrangChuController.php
@@ -279,10 +279,76 @@ class TrangChuController extends Controller
 
         $query = trim($request->input('query'));
 
-        $goiY = SanPham::where('ten_san_pham', 'like', '%' . $query . '%')
-            ->limit(10)
-            ->get();
+        $goiY =
+            SanPham::query()
+                ->select(
+                    'san_phams.id',
+                    'san_phams.ten_san_pham',
+                    'san_phams.duong_dan',
+                    'san_phams.anh_san_pham',
+                    'san_phams.hang_moi',
+                    'san_phams.gia_tot',
+                    DB::raw('MIN(CASE
+                    WHEN bien_the_san_phams.gia_khuyen_mai_tam_thoi IS NOT NULL THEN bien_the_san_phams.gia_khuyen_mai_tam_thoi
+                    WHEN bien_the_san_phams.gia_khuyen_mai IS NOT NULL THEN bien_the_san_phams.gia_khuyen_mai
+                    ELSE bien_the_san_phams.gia_ban
+                END) as gia_thap_nhat'),
 
+                    DB::raw('MAX(CASE
+                    WHEN bien_the_san_phams.gia_khuyen_mai_tam_thoi IS NOT NULL THEN bien_the_san_phams.gia_khuyen_mai_tam_thoi
+                    WHEN bien_the_san_phams.gia_khuyen_mai IS NOT NULL THEN bien_the_san_phams.gia_khuyen_mai
+                    ELSE bien_the_san_phams.gia_ban
+                END) as gia_cao_nhat')
+                )
+                ->join('bien_the_san_phams', 'san_phams.id', '=', 'bien_the_san_phams.san_pham_id')
+                ->join('bien_the_mau_sacs', 'bien_the_san_phams.bien_the_mau_sac_id', '=', 'bien_the_mau_sacs.id')
+                ->join('bien_the_kich_thuocs', 'bien_the_san_phams.bien_the_kich_thuoc_id', '=', 'bien_the_kich_thuocs.id')
+                ->where('san_phams.trang_thai', 1)
+                ->where('san_phams.hang_moi', 1)
+                ->whereNotNull('san_phams.danh_muc_id')
+                ->where('ten_san_pham', 'like', '%' . $query . '%')
+                ->groupBy('san_phams.id', 'san_phams.ten_san_pham', 'san_phams.duong_dan', 'san_phams.anh_san_pham')
+                ->orderByDesc('san_phams.id')
+                ->take(8)
+                ->get()
+                ->map(function ($sanPham) {
+                    $bienThe = BienTheSanPham::query()
+                        ->with('anhBienThe')
+                        ->select(
+                            'bien_the_san_phams.id',
+                            'bien_the_san_phams.san_pham_id',
+                            'bien_the_san_phams.so_luong_bien_the',
+                            'bien_the_mau_sacs.ten_mau_sac',
+                            'bien_the_mau_sacs.ma_mau_sac',
+                            'bien_the_kich_thuocs.kich_thuoc',
+                            DB::raw('bien_the_san_phams.gia_ban as gia_chua_giam'),
+                            DB::raw('CASE
+                            WHEN bien_the_san_phams.gia_khuyen_mai_tam_thoi IS NOT NULL THEN bien_the_san_phams.gia_khuyen_mai_tam_thoi
+                            WHEN bien_the_san_phams.gia_khuyen_mai IS NOT NULL THEN bien_the_san_phams.gia_khuyen_mai
+                            ELSE bien_the_san_phams.gia_ban
+                        END as gia_hien_tai')
+                        )
+                        ->join('bien_the_mau_sacs', 'bien_the_san_phams.bien_the_mau_sac_id', '=', 'bien_the_mau_sacs.id')
+                        ->join('bien_the_kich_thuocs', 'bien_the_san_phams.bien_the_kich_thuoc_id', '=', 'bien_the_kich_thuocs.id')
+                        ->where('bien_the_san_phams.san_pham_id', $sanPham->id)
+                        ->get();
+
+                    $mauSacVaAnh = $bienThe->groupBy('ma_mau_sac')->map(function ($items) {
+                        $bienTheDauTien = $items->first();
+                        $anhBienTheDaiDien = $bienTheDauTien->anhBienThe->first() ? $bienTheDauTien->anhBienThe->first()->duong_dan_anh : null;
+
+                        return [
+                            'ma_mau_sac' => $bienTheDauTien->ma_mau_sac,
+                            'ten_mau_sac' => $bienTheDauTien->ten_mau_sac,
+                            'hinh_anh' => $anhBienTheDaiDien
+                        ];
+                    })->values()->all();
+
+                    $sanPham->bien_the = $bienThe;
+                    $sanPham->mau_sac_va_anh = $mauSacVaAnh;
+
+                    return $sanPham;
+                });
         return response()->json($goiY);
     }
 
