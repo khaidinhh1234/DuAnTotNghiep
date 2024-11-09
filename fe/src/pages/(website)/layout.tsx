78b16cfd818de4db7a20fa5ef308f55d4ed4a84b
Nguyễn Chí Đức <ducncph32766@fpt.edu.vn>
2024-11-09 21:49:41 +0700

    fixthanhtoan

--- fe/src/pages/(website)/layout.tsx
+++ fe/src/pages/(website)/layout.tsx
@@ -11,9 +11,9 @@ const Layout = () => {
 
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       {/* <Test/> */}
-      {/* <Header2 /> */}
+      <Header2 />
       <Outlet />
       <Footer />
     </div>
