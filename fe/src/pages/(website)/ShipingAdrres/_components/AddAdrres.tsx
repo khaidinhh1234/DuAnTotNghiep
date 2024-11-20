2f6794a8234e1a82678f3b0a84bd036039dd8933
kien48 <kienltph35295@fpt.edu.vn>
2024-11-20 23:32:01 +0700

    sửa giao diện

--- fe/src/pages/(website)/ShipingAdrres/_components/AddAdrres.tsx
+++ fe/src/pages/(website)/ShipingAdrres/_components/AddAdrres.tsx
@@ -5,7 +5,7 @@ const AddAddressForm = ({ register, products, errors, settrangthai }: any) => {
 
   return (
     <div className="">
-      <h3 className="title-h3">Sản phẩm đã đặt</h3>
+      <h3 className="title-h3">Sản phẩm đã chọn</h3>
       {/* <div className="px-2 mb-8">
         {[1, 2, 3].map((index) => (
           <div
