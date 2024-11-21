ed136eb2d3b1f9f67e877e8299ee19d04c99a570
ntoarn <nqton301004@gmail.com>
2024-11-21 15:13:07 +0700

    linhtinh

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -4,20 +4,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { StyleProvider } from "@ant-design/cssinjs";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import "./global.css";
-// import "./assets/js/index.js";
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import "./assets/js/index.js
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>   
         <StyleProvider layer>
           <App />
         </StyleProvider>
-      </QueryClientProvider>
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
