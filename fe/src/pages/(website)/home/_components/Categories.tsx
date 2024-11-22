b62a2019e8ff92ec7e8fcf83d02110fc18681f65
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-11-22 22:08:15 +0700

    pull

--- fe/src/pages/(website)/home/_components/Categories.tsx
+++ fe/src/pages/(website)/home/_components/Categories.tsx
@@ -2,8 +2,51 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import { useState } from "react";
 import "swiper/css";
-const   Categories = ({ bo_suu_tap }: any) => {
+import { Link } from "react-router-dom";
+import View from "../../_component/View";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import instanceClient from "@/configs/client";
+import { message } from "antd";
+import { Tag, Ticket } from "lucide-react";
+const Categories = ({ bo_suu_tap }: any) => {
   const [activeTab, setActiveTab] = useState<number>(0);
+  const queryclient = useQueryClient();
+  const [hoveredProductId, setHoveredProductId] = useState<number | null>(null);
+  const [hoveredVariantIndex, setHoveredVariantIndex] = useState<number | null>(
+    null
+  );
+  const handleMouseEnter = (productId: number, variantIndex: any) => {
+    setHoveredProductId(productId);
+    setHoveredVariantIndex(variantIndex);
+  };
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (id: any) => {
+      try {
+        const response = await instanceClient.post(`sanpham/yeuthich/${id}`);
+
+        if (
+          response.data.mess === "Sản phẩm đã được xóa khỏi danh sách yêu thích"
+        ) {
+          message.success("Xóa sản phẩm yêu thích thành công");
+        }
+        if (
+          response.data.mess === "Sản phẩm đã được thêm vào danh sách yêu thích"
+        ) {
+          message.success("Thêm sản phẩm yêu thích thành công");
+        }
+
+        return response.data;
+      } catch (error: any) {
+        message.error(error?.response?.data?.mess);
+        // throw new Error("Xóa sản phẩm yêu thích thất bại");
+      }
+    },
+    onSuccess: () => {
+      queryclient.invalidateQueries({
+        queryKey: ["TRANG_CHU_CLIENT"],
+      });
+    },
+  });
   // const categories = [
   //   {
   //     name: "Kids",
@@ -35,7 +78,7 @@ const   Categories = ({ bo_suu_tap }: any) => {
         <div className="md:container md:mb-28  mb-16  container-mobile">
           <div className="md:mb-14 mb-8 mx-2 md:mx-0">
             <h1 className="md:text-4xl text-3xl font-semibold tracking-[1px] text-center">
-              Bộ Sưu Tập Bán Chạy
+              Bộ Sưu Tập
             </h1>{" "}
             <div className="flex justify-center items-center  my-10">
               {bo_suu_tap?.map((item: any, index: number) => (
@@ -47,70 +90,132 @@ const   Categories = ({ bo_suu_tap }: any) => {
                   }`}
                   key={index}
                   onClick={() => setActiveTab(index)}
+                  title={item?.ten}
                 >
                   {item?.ten}
                 </button>
               ))}
             </div>
           </div>
-          {/* {bo_suu_tap?.map((item: any, index: number) => ( */}
-          <Swiper
-            spaceBetween={100}
-            slidesPerView={4}
-            breakpoints={{
-              0: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
+          <div className="flex">
+            <div className="hidden lg:block w-1/4 pr-4">
+              <img
+                src={bo_suu_tap[activeTab]?.duong_dan_anh}
+                alt={bo_suu_tap[activeTab]?.ten}
+                className="w-full h-[550px] object-cover rounded-lg shadow-lg "
+              />
+            </div>
+            <div className="w-full lg:w-3/4">
+              <Swiper
+                spaceBetween={140}
+                slidesPerView={4}
+                breakpoints={{
+                  0: { slidesPerView: 1, spaceBetween: 10 },
+                  640: { slidesPerView: 1.5, spaceBetween: 30 },
+                  1024: { slidesPerView: 3 },
+                  1244: { slidesPerView: 4 },
+                }}
+              >
+                {bo_suu_tap[activeTab]?.san_phams?.map(
+                  (item: any, index: number) => (
+                    <SwiperSlide
+                      key={index}
+                      className="group relative mx-2 lg:mx-0"
+                    >
+                      <div className="relative">
+                        <span
+                          onClick={() => mutate(item?.id)}
+                          className={`z-10 text-xl bg-white w-11 h-11 flex items-center justify-center absolute top-3 right-6 rounded-full transition-all duration-300 ${
+                            item?.yeu_thich
+                              ? "text-red-500"
+                              : "text-black hover:text-white"
+                          } opacity-0 invisible group-hover:opacity-100 group-hover:visible hover:bg-black`}
+                        >
+                          <i className="fa-solid fa-heart" />
+                        </span>
+                        <Link to={`/product-detail/${item.duong_dan}`}>
+                          <div className="w-[400px] lg:w-[300px] lg:h-[400px] h-[500px] bg-neutral-200/70 relative">
+                            <img
+                              src={item.anh_san_pham}
+                              alt={item.ten_san_pham}
+                              className="absolute top-0 left-0 w-full h-full object-cover z-10 rounded-t-md"
+                            />
+                            {item.trong_chuong_trinh_uu_dai && (
+                              <div
+                                className="absolute bottom-0 left-0 text-white px-4 py-2 rounded-tr-2xl text-sm font-bold z-20 flex items-center space-x-2"
+                                style={{
+                                  background:
+                                    "linear-gradient(to right, #f857a6, #ff5858)",
+                                }}
+                              >
+                                <Ticket className="w-4 h-4" />
+                                <div>
+                                  <div>{item.trong_chuong_trinh_uu_dai}</div>
+                                </div>
+                              </div>
+                            )}
+                          </div>
+                        </Link>
+                        <View id={item?.duong_dan} ID={item?.id} />
+                      </div>
+                      <div className="bg-slate-50 pt-4 px-4 rounded-md pb-2 lg:w-[300px]">
+                        <Link to={`/product-detail/${item.duong_dan}`}>
+                          <h5 className="text-base truncate w-60 font-medium hover:text-red-500 text-left">
+                            {item?.ten_san_pham}
+                          </h5>
+                        </Link>
+                        <p className="font-semibold text-lg text-left">
+                          {item?.gia_thap_nhat === item?.gia_cao_nhat ? (
+                            <>
+                              {(item?.gia_cao_nhat ?? 0).toLocaleString(
+                                "vi-VN"
+                              )}{" "}
+                              đ
+                            </>
+                          ) : (
+                            <>
+                              {(item?.gia_thap_nhat ?? 0).toLocaleString(
+                                "vi-VN"
+                              )}{" "}
+                              đ
+                              <i className="fa-solid fa-minus text-sm mx-1 text-slate-500"></i>
+                              {(item?.gia_cao_nhat ?? 0).toLocaleString(
+                                "vi-VN"
+                              )}{" "}
+                              đ
+                            </>
+                          )}
+                        </p>
 
-              640: {
-                slidesPerView: 1.5,
-                spaceBetween: 30,
-              },
-              840: {
-                slidesPerView: 1.5,
-                spaceBetween: 30,
-              },
-              1024: {
-                slidesPerView: 3,
-              },
-              1204: {
-                slidesPerView: 3,
-              },
-              1244: {
-                slidesPerView: 4,
-              },
-            }}
-            // onSlideChange={() => console.log("slide change")}
-            // onSwiper={(swiper) => console.log(swiper)}
-            className="grid grid-flow-col lg:auto-cols-[100px]  md:auto-cols-[50px]  auto-cols-[40px] gap-x-[12px] lg:gap-x-[22px] "
-          >
-            {bo_suu_tap[activeTab]?.san_phams?.map(
-              (item: any, index: number) => (
-                <SwiperSlide
-                  style={{ width: "300px" }}
-                  key={index}
-                  className="mx-2  lg:mx-0 "
-                >
-                  <div className="">
-                    <div className="w-[400px]  lg:w-[300px] lg:h-[400px] h-[500px] bg-neutral-200/70 relative ">
-                      <h1 className="text-neutral-300 absolute pt-5  truncate flex items-center justify-center px-6 text-7xl font-bold text-center z-0 ">
-                        {item?.ten_san_pham?.substring(0, 5)}
-                      </h1>
-                      <img
-                        src={item?.anh_san_pham}
-                        alt=""
-                        className="absolute top-0 left-0 w-full h-full object-cover z-10"
-                      />
-                      <button className="hover:bg-black hover:text-white bg-white text-center lg:w-[270px] w-[360px] py-[13px] absolute bottom-6 left-4 rounded-lg  text-base z-20 font-semibold truncate px-10">
-                        {item?.ten_san_pham}
-                      </button>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              )
-            )}
-          </Swiper>
+                        <p className="font-bold text-lg flex items-center">
+                          {item?.mau_sac_va_anh?.map(
+                            (variant: any, index: number) => (
+                              <button
+                                key={index}
+                                className={`w-7 h-7 rounded-full border mr-1 ${
+                                  hoveredProductId === item?.id &&
+                                  hoveredVariantIndex === index
+                                    ? "border-black"
+                                    : "border-gray-300 hover:border-black"
+                                }`}
+                                style={{
+                                  backgroundColor: variant?.ma_mau_sac,
+                                }}
+                                title={variant?.ten_mau_sac}
+                                onMouseEnter={() =>
+                                  handleMouseEnter(item?.id, index)
+                                }
+                              />
+                            )
+                          )}
+                        </p>
+                      </div>
+                    </SwiperSlide>
+                  )
+                )}
+              </Swiper>
+            </div>
+          </div>
         </div>
       </section>
     </>
