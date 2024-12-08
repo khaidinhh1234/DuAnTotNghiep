0d4637a67febdd868c8dd1a7ed567fc1925c8e05
tanloi28 <loiktph32675@fpt.edu.vn>
2024-12-08 21:46:44 +0700

    fix

--- fe/src/pages/(website)/Shopuudai/_components/AllProduct.tsx
+++ fe/src/pages/(website)/Shopuudai/_components/AllProduct.tsx
@@ -1,18 +1,31 @@
 
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import instanceClient from "@/configs/client";
 
 const AllProduct = () => {
   const { slug } = useParams();
 
+  const { data: promotions } = useQuery({
+    queryKey: ["promotions"],
+    queryFn: async () => {
+      const response = await instanceClient.get("/chuong-trinh-uu-dai");
+      return response.data.data;
+    },
+  });
+
+  const currentPromotion = promotions?.find(
+    (promo : any) => promo.duong_dan === slug
+  );
 
   return (
     <>
       <section>
         <div className="container">
-          <div className=" flex mt-[70px] mb-9">
+          <div className="flex mt-[70px] mb-9">
             <p className="pr-2">Sản phẩm</p>
             --
-            <p className="px-2">{slug}</p>
+            <p className="px-2">{currentPromotion?.ten_uu_dai || "Khuyến mãi"}</p>
           </div>
         </div>
       </section>
