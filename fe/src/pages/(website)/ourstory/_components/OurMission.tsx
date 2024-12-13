ed39cf9cc653c626affd4062fda76ba557275267
ntoarn <nqton301004@gmail.com>
2024-12-13 16:49:10 +0700

    responsive

--- fe/src/pages/(website)/ourstory/_components/OurMission.tsx
+++ fe/src/pages/(website)/ourstory/_components/OurMission.tsx
@@ -4,16 +4,17 @@ const OurMission = () => {
   return (
     <>
       <section id="OurMission" className="mb-16 bg-neutral-200 pt-10 pb-20">
-        <div className="container">
+        <div className="container mx-auto px-4">
           <h2 className="lg:text-4xl text-2xl font-semibold mb-10 text-center">
             Sứ Mệnh Của Chúng Tôi
           </h2>
-          <div className="grid grid-cols-12 gap-10">
-            <div className="col-span-6">
-              <h3 className="lg:text-[40px] text-2xl font-semibold py-3">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-10">
+            {/* Cột bên trái */}
+            <div className="lg:col-span-6 flex flex-col items-center lg:items-start">
+              <h3 className="lg:text-[40px] text-2xl font-semibold py-3 text-center lg:text-left">
                 Thời Trang và Sự Thoải Mái
               </h3>
-              <p className="lg:text-lg lg:max-w-lg my-3 font-medium">
+              <p className="lg:text-lg lg:max-w-lg my-3 font-medium text-center lg:text-left">
                 Chúng tôi cam kết mang đến cho khách hàng những trang phục thời
                 trang và thoải mái. Mục tiêu của chúng tôi là kết hợp xu hướng
                 thời trang với sự thoải mái hàng ngày, đảm bảo rằng trang phục
@@ -22,19 +23,20 @@ const OurMission = () => {
               <img
                 src={our1}
                 alt=""
-                className="lg:w-[560px] lg:h-[648px] w-[260px] h-[348px] py-3"
+                className="lg:w-[560px] lg:h-[648px] w-full h-auto py-3"
               />
             </div>
-            <div className="col-span-6">
+            {/* Cột bên phải */}
+            <div className="lg:col-span-6 flex flex-col items-center lg:items-start">
               <img
                 src={our2}
                 alt=""
-                className="lg:w-[560px] lg:h-[648px] w-[260px] h-[348px] py-3"
+                className="lg:w-[560px] lg:h-[648px] w-full h-auto py-3"
               />
-              <h3 className="lg:text-[40px] text-2xl font-semibold py-3">
+              <h3 className="lg:text-[40px] text-2xl font-semibold py-3 text-center lg:text-left">
                 Chất Lượng và Sự Tự Tin
               </h3>
-              <p className="lg:text-lg lg:max-w-lg my-3 font-medium">
+              <p className="lg:text-lg lg:max-w-lg my-3 font-medium text-center lg:text-left">
                 Chúng tôi nỗ lực mang đến những trang phục chất lượng cao,
                 truyền cảm hứng tự tin. Mỗi sản phẩm được chế tác với sự chú ý
                 tỉ mỉ đến từng chi tiết, đảm bảo không chỉ vẻ đẹp thẩm mỹ mà còn
