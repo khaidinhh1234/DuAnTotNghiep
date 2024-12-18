b36e51975e3e4f082e4daa90ff32e1d2bfb6c204
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-12-18 17:16:08 +0700

    pull

--- be/app/Http/Controllers/Admin/Api/LichSuHoatDongController.php
+++ be/app/Http/Controllers/Admin/Api/LichSuHoatDongController.php
@@ -32,6 +32,8 @@ class LichSuHoatDongController extends Controller
             ->join('vai_tro_tai_khoan', 'users.id', '=', 'vai_tro_tai_khoan.user_id')
             ->join('vai_tros', 'vai_tro_tai_khoan.vai_tro_id', '=', 'vai_tros.id')
             ->where('vai_tros.ten_vai_tro', '!=', 'Khách hàng')
+            ->where('vai_tros.ten_vai_tro', '!=', 'Quản trị viên')
+
             ->where('vai_tros.ten_vai_tro', '!=', 'Người giao hàng');
 
         if ($user->vaiTros()->first()->ten_vai_tro == 'Quản trị viên') {
